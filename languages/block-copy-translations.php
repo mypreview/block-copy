2f502aa8eb2171b390bace94e1c08fda4ae92e43
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-03-03 20:10:44 +0300

    Updated

--- languages/block-copy-translations.php
+++ languages/block-copy-translations.php
@@ -1,10 +1,10 @@
 <?php
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 $generated_i18n_strings = array(
-	// Reference: assets/src/utils/withDispatch.js:23
-	_n_noop( 'A single block copied.', '%s blocks copied.', 'block-copy' ),
+	// Reference: assets/src/components/Copy.jsx:65
+	_n_noop( 'Copy Block', 'Copy %s Blocks', 'block-copy' ),
 
-	// Reference: assets/src/components/Copy.jsx:64
-	_x( 'Copy Block', 'button label', 'block-copy' )
+	// Reference: assets/src/utils/withDispatch.js:23
+	_n_noop( 'A single block copied.', '%s blocks copied.', 'block-copy' )
 );
 /* THIS IS THE END OF THE GENERATED FILE */
